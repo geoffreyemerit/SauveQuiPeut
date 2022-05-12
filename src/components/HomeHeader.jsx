f15b2418196f3b521a7fce08e48998a342a5535c
Quentin GUERANDEL <quentin.guerandel@gmail.com>
2022-05-12 16:51:22 +0200

    finalized headerHome

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomeHeader = () => {
   return (
-    <div className="flex">
-      <div className="pl-12">
+    <div className="flex h-screen">
+      <div className="pl-12 mt-20">
         <h1 className="font-extrabold text-8xl text-transparent bg-clip-text bg-gradient-to-br from-bg-left to-bg-right py-3">
           Sauve Qui Peut
         </h1>
@@ -14,8 +15,27 @@ const HomeHeader = () => {
         <h2 className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-slate-200 to-white py-3">
           Première plateforme de vente de NFT <br /> à impact écologique positif
         </h2>
+        <div className="flex pt-64">
+          <img
+            src="../public/assets/global/play.png"
+            alt="knowMore"
+            width={20}
+            height={20}
+          />
+          <Link className="hover:text-gray-200" to="/ecostats">
+            <h4 className="text-sm font-bold text-slate-200 pl-2">
+              En savoir plus sur nos éco-stats!
+            </h4>
+          </Link>
+        </div>
       </div>
-      <img src="../public/assets/global/grenadeBg.png" alt="SQP" width={600} />
+      <div className="pr-4">
+        <img
+          src="../public/assets/global/grenadeBg.png"
+          alt="SQP"
+          width={800}
+        />
+      </div>
     </div>
   );
 };
