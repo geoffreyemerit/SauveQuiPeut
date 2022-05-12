f6544e13c86b645a28fa56ac0ccae9e3556baa26
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2022-05-13 01:46:03 +0200

    card bees page mapping ok

--- src/pages/Bee.jsx
+++ src/pages/Bee.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BeeHeader from "../components/BeeHeader";
+import BeeList from "../components/BeeList";
 
 const Bee = () => {
   return (
     <div>
       <BeeHeader />
+      <BeeList />
     </div>
   );
 };
