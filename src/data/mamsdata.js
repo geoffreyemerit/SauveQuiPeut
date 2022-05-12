9f64b8fb9e8df4d813e2cdb682b873ed98f05b19
Gavinus <96060707+Gavinus@users.noreply.github.com>
2022-05-12 17:27:38 +0200

    test push de hurryup

---
+++ src/data/mamsdata.js
@@ -0,0 +1,67 @@
+const mamsData = [
+  {
+    id: 0,
+    name: "RhinoferoceBlanc",
+    age: "60 years old",
+    image: "./assets/rhinos/0.png",
+    price: "26.01 ETH",
+  },
+  {
+    id: 1,
+    name: "VautourAfricain",
+    age: "30 years old",
+    image: "./assets/rhinos/1.png",
+    price: "0.218 ETH",
+  },
+  {
+    id: 2,
+    name: "GorillePlaine",
+    age: "40 years old",
+    image: "./assets/rhinos/2.png",
+    price: "0.998 ETH",
+  },
+  {
+    id: 3,
+    name: "CrocodileOrenoque",
+    age: "100 years old",
+    image: "./assets/rhinos/3.png",
+    price: "2.60 ETH",
+  },
+  {
+    id: 4,
+    name: "ChatAndes",
+    age: "20 years old",
+    image: "./assets/rhinos/4.png",
+    price: "5 ETH",
+  },
+  {
+    id: 5,
+    name: "LoupEthopie",
+    age: "15 years old",
+    image: "./assets/rhinos/5.png",
+    price: "18.241 ETH",
+  },
+  {
+    id: 6,
+    name: "Okapi",
+    age: "7 years old",
+    image: "./assets/rhinos/6.png",
+    price: "14.050 ETH",
+  },
+  {
+    id: 7,
+    name: "ChauveSourieFantome",
+    age: "39 years old",
+    image: "./assets/rhinos/7.png",
+    price: "10.200 ETH",
+  },
+  {
+    id: 8,
+    name: "Quokka",
+    age: "10 years old",
+    image: "./assets/rhinos/mainRhino.png",
+    price: "25 ETH",
+  },
+];
+
+export default mamsData;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,24 @@ import React from "react";
 
 import HomeHeader from "../components/HomeHeader";
 import HomeEcoStats from "../components/HomeEcoStats";
+<<<<<<< Updated upstream
 import HomeConvictions from "../components/HomeConvictions";
+=======
+import HurryUp from "../components/HurryUp";
+>>>>>>> Stashed changes
 
 const Home = () => {
   return (
     <div>
       <HomeHeader />
+<<<<<<< Updated upstream
       <HomeConvictions />
       <HomeEcoStats />
+=======
+      <HurryUp />
+      <HomeEcoStats />
+      <Footer />
+>>>>>>> Stashed changes
     </div>
   );
 };
