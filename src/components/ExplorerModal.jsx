9ff48621393e62fad1af91b92a8673206fcb07df
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2022-05-13 08:13:35 +0200

    modal ok

--- src/components/ExplorerModal.jsx
+++ src/components/ExplorerModal.jsx
@@ -13,7 +13,7 @@ const ExplorerModal = ({
   return (
     // >> Modal decoration
     <div className="z-[20] fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[100vw] h-[100vh] bg-slate-900 flex justify-center items-center bg-opacity-10 backdrop-blur-[2px]">
-      <div className="flex flex-col items-center justify-center w-3/5 rounded-lg box-shadow-1">
+      <div className="flex flex-col items-center w-[54rem] justify-center rounded-lg box-shadow-1">
         <img
           src={image}
           alt={id}
@@ -28,16 +28,22 @@ const ExplorerModal = ({
         >
           X
         </button>
-        <div className="absolute z-[20] rounded-md mt-[25rem] w-[54rem] bg-white h-36">
-          <h3 className="text-center absolute z-[20] font-bold text-2xl text-slate-900 -mb-[300px] bp1:text-[1rem] bp2:-mb-[100px]">
+        <div className="absolute z-[20] rounded-md mt-[30rem] w-[54rem] bg-slate-900 h-20">
+          <h3 className="text-center ml-2 absolute z-[20] font-bold text-3xl text-slate-50 -mb-[300px] bp1:text-[1rem] bp2:-mb-[100px]">
             {name}
           </h3>
-          <span>{autor}</span>
-          <span>Age : {age}</span>
-          <span>Price : {price}</span>
+          <span className="absolute z-[20] ml-2 mt-9 text-xs text-slate-50">
+            {autor}
+          </span>
+          <span className="absolute z-[20] ml-2 mt-14 text-xs text-slate-50">
+            Age : {age}
+          </span>
+          <span className="absolute z-[20] ml-[43rem] mt-1 text-md text-slate-50">
+            Price : {price}
+          </span>
           <button
             type="button"
-            className="absolute font-orb z-[20] font-medium text-slate-900 bg-opacity-40 rounded-md backdrop-blur-[2px] border-2 border-black text-2xl -mb-[200px] bp1:text-[1.5vw] bp2:-mb-[230px] bp2:text-[2.4vw] bp3:text-[4.2vw] box-shadow-2 bg-slate-50 px-6 hover:scale-[1.01]"
+            className="absolute ml-[40rem] mt-8 font-orb z-[20] font-medium text-slate-50 bg-opacity-40 rounded-md backdrop-blur-[2px] border-2 border-slate-50 text-2xl  bp1:text-[1.5vw] bp2:-mb-[230px] bp2:text-[2.4vw] bp3:text-[4.2vw] box-shadow-2 bg-slate-50 px-6 hover:scale-[1.01]"
           >
             Acheter ce NFT
           </button>
