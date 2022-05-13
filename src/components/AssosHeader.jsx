d5dfae111187b35a56acf8edfd9828bcea9c0891
unknown <mrbreenalexis@gmail.com>
2022-05-13 10:18:13 +0200

    modifs eco stats

--- src/components/AssosHeader.jsx
+++ src/components/AssosHeader.jsx
@@ -8,12 +8,12 @@ const AssosHeader = () => {
           ASSOCIATION ECOLOGY
         </h1>
         <h2 className="py-3 text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-slate-600 to-white">
-          Voici un panel d&aquos;associations agissant pour la planète.
+          Voici un panel d&apos;associations agissant pour la planète.
         </h2>
         <h2 className="py-3 text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-slate-200 to-white">
           20 associations pour vous !
         </h2>
-        <div className="w-[33rem] h-[3px] bg-gradient-to-r from-[#73E0A9] to-[#5240f5]" />
+        <div className="w-[38rem] h-[3px] bg-gradient-to-r from-[#73E0A9] to-[#5240f5]" />
       </div>
       <div className="pr-4">
         <img
