54b7e9058a830d5d299908895fc5687aad61c196
Geoffrey Emerit <geoffrey.emerit@gmail.com>
2022-05-13 02:08:20 +0200

    page rhinos mappeing + card

--- src/pages/Rhinos.jsx
+++ src/pages/Rhinos.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import RhinosHeader from "../components/RhinosHeader";
+import RhinoList from "../components/RhinoList";
 
 const Rhinos = () => {
   return (
     <div>
       <RhinosHeader />
+      <RhinoList />
     </div>
   );
 };
